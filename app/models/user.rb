aa5ae4587b629ca2741fc79ccfa6350bf84f8437
kohei.fukada <kofee.ruby@gmail.com>
2020-06-21 14:08:11 +0000

    add favorite

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,9 @@ class User < ApplicationRecord
   has_many :followings, through: :relationships, source: :follow
   has_many :reverses_of_relationship, class_name: 'Relationship', foreign_key: 'follow_id'
   has_many :followers, through: :reverses_of_relationship, source: :user
+  
+  has_many :favorites 
+  has_many :likes, through: :favorites, source: :micropost
 
   def follow(other_user)
     unless self == other_user
@@ -29,4 +32,18 @@ class User < ApplicationRecord
   def feed_microposts
     Micropost.where(user_id: self.following_ids + [self.id])
   end
+  
+  def favorite(favorite_micropost)
+    self.favorites.find_or_create_by(micropost_id: favorite_micropost.id)
+  end
+  
+  def unfavorite(favorite_micropost)
+    favorite = self.favorites.find_by(micropost_id: favorite_micropost.id)
+    favorite.destroy if favorite
+  end
+  
+  def favorite?(favorite_micropost)
+    self.likes.include?(favorite_micropost)
+  end
+  
 end
